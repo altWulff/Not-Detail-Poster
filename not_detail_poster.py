61b7e0907e4cf9ff90b8689e98e8b66d63a7d8b7
Oleg Repalo <altwulf7@gmail.com>
2021-10-16 15:32:54 +0300

    Work on shell

--- not_detail_poster.py
+++ not_detail_poster.py
@@ -1,12 +1,28 @@
 import click
 from werkzeug.security import generate_password_hash
 from app import app, db, user_datastore
-from app.models import Barista, DailyReport, Role
+from app.models import Barista, DailyReport, Role, CoffeeShop, CoffeeShopEquipment
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'Barista': Barista, 'DailyReport': DailyReport}
+    return {
+        'db': db,
+        'Barista': Barista,
+        'DailyReport': DailyReport,
+        'Role': Role,
+        'CoffeeShop': CoffeeShop,
+        'CoffeeShopEquipment': CoffeeShopEquipment
+    }
+
+
+def create_roles():
+    db.create_all()
+    user_datastore.create_role(name='admin', description='all permissions')
+    user_datastore.create_role(name='moderator', description='moderator permissions')
+    user_datastore.create_role(name='user', description='user permissions')
+    db.session.commit()
+    print('Create roles: user, moderator, admin')
 
 
 @app.cli.command("createsuperuser")
@@ -17,10 +33,11 @@ def create_superuser(username, password):
     name = username
     password = generate_password_hash(password)
     user_datastore.create_user(name=name, password_hash=password)
-    user_datastore.create_role(name='admin', description='all permissions')
+    create_roles()
     user = Barista.query.filter_by(name=name).first()
     role = Role.query.filter_by(name='admin').first()
     user_datastore.activate_user(user)
     user_datastore.add_role_to_user(user, role)
     db.session.commit()
-    print (f'Create superuser with name: {name}')
+    print(f'Create superuser with name: {name}')
+
