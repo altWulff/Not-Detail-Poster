f845754347c444e4472098fabb4863e5d3790f50
altWulff <altwulf7@gmail.com>
2021-10-19 11:44:01 +0300

    Base permisions to admin panel

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,11 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_admin import Admin
 from flask_moment import Moment
-from flask_admin.contrib.sqla import ModelView
+#from flask_admin.contrib.sqla import ModelView
 from flask_security import Security, SQLAlchemyUserDatastore
 from flask_debugtoolbar import DebugToolbarExtension
 from config import DevelopmentConfig, ProductionConfig
-
+from flask_admin import helpers as admin_helpers
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
@@ -22,16 +22,23 @@ admin = Admin(app, name='Not Detail Poster', template_mode='bootstrap4')
 toolbar = DebugToolbarExtension(app)
 moment = Moment(app)
 
-from app import routes, models, forms, admin, routes
+from app import routes, models, forms, routes
 from app.routes import errors
 from app.routes.user import user
 from app.routes.menu import menu
 from app.routes.report import report
 from app.routes.errors import errors
-
+from app.admin_view import MyModelView as ModelView
 user_datastore = SQLAlchemyUserDatastore(db, models.Barista, models.Role)
 security = Security(app, user_datastore)
 
+@security.context_processor
+def security_context_processor():
+    return dict(
+        admin_base_template=admin.base_template,
+        admin_view=admin.index_view,
+        h=admin_helpers,
+    )
 app.register_blueprint(user)
 app.register_blueprint(menu)
 app.register_blueprint(report)
---
+++ app/admin_view.py
@@ -0,0 +1,22 @@
+from flask import abort, redirect, request, url_for
+from flask_security import current_user
+from flask_admin.contrib import sqla
+
+
+
+class MyModelView(sqla.ModelView):
+    def is_accessible(self):
+        return (current_user.is_active and current_user.is_authenticated and current_user.has_role('admin')
+        )
+
+    def _handle_view(self, name, **kwargs):
+        """
+        Override builtin _handle_view in order to redirect users when a view is not accessible.
+        """
+        if not self.is_accessible():
+            if current_user.is_authenticated:
+                # permission denied
+                abort(403)
+            else:
+                # login
+                return redirect(url_for('login', next=request.url))
