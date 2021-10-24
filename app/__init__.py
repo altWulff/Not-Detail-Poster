0b9659b6b30c770904c3c993f34c8a698de2eb88
Oleg Repalo <altwulf7@gmail.com>
2021-10-24 22:19:20 +0300

    Work on admin panel, localization, and add summary to daily report

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,10 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_admin import Admin
 from flask_moment import Moment
-#from flask_admin.contrib.sqla import ModelView
 from flask_security import Security, SQLAlchemyUserDatastore
 from flask_debugtoolbar import DebugToolbarExtension
-from config import DevelopmentConfig, ProductionConfig
 from flask_admin import helpers as admin_helpers
+from config import DevelopmentConfig, ProductionConfig
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
@@ -23,13 +22,13 @@ toolbar = DebugToolbarExtension(app)
 moment = Moment(app)
 
 from app import routes, models, forms, routes
+from app.routes import auth
 from app.routes import errors
 from app.routes.user import user
 from app.routes.menu import menu
 from app.routes.report import report
 from app.routes.errors import errors
-from app.admin_view import MyModelView as ModelView
-from app.admin_view import BaristaAdmin, DailyReportAdmin
+from app.admin_view import ModelView, CoffeeShopAdmin, CoffeeShopEquipmentAdmin, WarehouseAdmin, DailyReportAdmin, BaristaAdmin, ReportsAdminView, RoleAdmin
 user_datastore = SQLAlchemyUserDatastore(db, models.Barista, models.Role)
 security = Security(app, user_datastore)
 
@@ -47,17 +46,17 @@ app.register_blueprint(user)
 app.register_blueprint(menu)
 app.register_blueprint(report)
 
-admin.add_view(ModelView(models.CoffeeShop, db.session, name='Все кофейни', category="Кофейни"))
-admin.add_view(ModelView(models.CoffeeShopEquipment, db.session, name='Оборудование', category="Кофейни"))
-admin.add_view(ModelView(models.Warehouse, db.session,  name='Все товары', category="Склады"))
+admin.add_view(CoffeeShopAdmin(models.CoffeeShop, db.session, name='Все кофейни', category="Кофейни"))
+admin.add_view(CoffeeShopEquipmentAdmin(models.CoffeeShopEquipment, db.session, name='Оборудование', category="Кофейни"))
+admin.add_view(WarehouseAdmin(models.Warehouse, db.session,  name='Все товары', category="Склады"))
 admin.add_view(DailyReportAdmin(models.DailyReport, db.session, name='Отчеты', category='Статистика'))
 admin.add_view(ModelView(models.Supply, db.session,  name='Поступления', category="Движения товаров"))
 admin.add_view(ModelView(models.ByWeight, db.session,  name='Развес', category="Движения товаров"))
 admin.add_view(ModelView(models.WriteOff, db.session,  name='Списания', category="Движения товаров"))
 admin.add_view(ModelView(models.Expense, db.session, name='Расходы', category='Кассовые средства'))
 admin.add_view(BaristaAdmin(models.Barista, db.session, name='Сотрудники'))
-admin.add_view(ModelView(models.Role, db.session, name='Роли'))
-# admin.add_view(ModelView(models.expenses, db.session))
+admin.add_view(RoleAdmin(models.Role, db.session, name='Доступ'))
+admin.add_view(ReportsAdminView(name='Фильт отчетов', category='Статистика', endpoint='reports'))
 
 if not app.debug:
     if not os.path.exists('logs'):
--- app/admin_view.py
+++ app/admin_view.py
@@ -1,10 +1,14 @@
-from flask import abort, redirect, request, url_for
+from sqlalchemy import func
+from flask import abort, redirect, request, url_for, flash
 from flask_security import current_user
 from flask_admin.contrib import sqla
 from flask_admin.model.template import macro
+from flask_admin import BaseView, expose
+from app.models import DailyReport, CoffeeShop, Barista
+from app.admin_forms import ReportsFilterForm
 
 
-class MyModelView(sqla.ModelView):
+class ModelView(sqla.ModelView):
     def is_accessible(self):
         return (current_user.is_active and current_user.is_authenticated and current_user.has_role('admin')
                 )
@@ -22,13 +26,74 @@ class MyModelView(sqla.ModelView):
                 return redirect(url_for('login', next=request.url))
 
 
-class BaristaAdmin(MyModelView):
+class ReportsAdminView(BaseView):
+    @expose('/', methods=('POST', 'GET'))
+    def index(self):
+        form = ReportsFilterForm()
+        reports = DailyReport.query.all()
+        form.target_coffee_shop.choices = [(g.id, g.place_name) for g in CoffeeShop.query.order_by('place_name')]
+        form.target_barista.choices = [(g.id, g.name) for g in Barista.query.order_by('name')]
+        if request.method == "POST":
+            reports = DailyReport.query.filter_by(coffee_shop_id=form.target_coffee_shop.data).filter_by(barista_name=form.target_barista.data).first_or_404()
+            flash('...')
+            return self.render('admin/reports_filter.html', reports=reports, form=form)
+        return self.render('admin/reports_filter.html', reports=reports, form=form)
+
+
+class CoffeeShopAdmin(ModelView):
+    column_searchable_list = ('place_name', 'address')
+    column_labels = dict(
+        place_name='Название',
+        address='Адрес',
+        cash='Наличка',
+        cashless='Безнал'
+    )
+
+
+class CoffeeShopEquipmentAdmin(ModelView):
+    column_searchable_list = ('coffee_machine', )
+    column_labels = dict(
+        coffee_machine='Кофе Машина',
+        grinder_1='Кофемолка 1',
+        grinder_2='Кофемолка 2',
+        coffee_shop='Кофейня'
+    )
+
+
+class WarehouseAdmin(ModelView):
+    column_labels = dict(
+        coffee_arabika='Арабика',
+        coffee_blend='Бленд',
+        milk='Молоко',
+        panini='Панини',
+        hot_dogs='Хот-доги',
+        coffee_shop='Кофейня'
+    )
+    column_formatters = dict(
+        coffee_arabika=lambda v, c, m, p: f'{m.coffee_arabika} кг',
+        coffee_blend=lambda v, c, m, p: f'{m.coffee_blend} кг',
+        milk=lambda v, c, m, p: f'{m.milk} л',
+        panini=lambda v, c, m, p: f'{m.panini} шт.',
+        hot_dogs=lambda v, c, m, p: f'{m.hot_dogs} шт.',
+    )
+
+
+class BaristaAdmin(ModelView):
     column_filters = ('name', 'phone_number', 'email')
     column_searchable_list = ('name', 'phone_number', 'email')
-    column_exclude_list = ('password_hash', 'roles', 'daily_reports', 'id')
+    column_exclude_list = ('password_hash', 'roles', 'daily_reports', 'id', 'salary_rate')
+    column_labels = dict(
+        name='Имя',
+        phone_number='Тел.',
+        email='Емейл',
+        coffee_shop='Кофейня',
+        daily_reports='Отчеты',
+        roles='Доступ',
+    )
+    column_formatters = dict(confirmed_at=lambda v, c, m, p: m.confirmed_at.date().strftime("%d.%m.%Y"))
 
 
-class DailyReportAdmin(MyModelView):
+class DailyReportAdmin(ModelView):
     can_view_details = True
     column_default_sort = ('timestamp', True)
     column_searchable_list = ('timestamp', )
@@ -64,6 +129,72 @@ class DailyReportAdmin(MyModelView):
         milk='Молоко/ост.',
         panini='Панини/ост.',
         hot_dogs='Хот-доги/ост.',
-        
-        )
+    )
     column_formatters = dict(timestamp=lambda v, c, m, p: m.timestamp.date().strftime("%d.%m.%Y"))
+    list_template = 'admin/model/custom_list.html'
+    page_size = 5
+
+    def page_cashbox(self, current_page):
+        # this should take into account any filters/search inplace
+        _query = self.get_query().limit(self.page_size).offset(current_page * self.page_size)
+        return sum([p.cashbox for p in _query])
+
+    def total_cashbox(self):
+        # this should take into account any filters/search inplace
+        return self.session.query(func.sum(DailyReport.cashbox)).scalar()
+
+    def page_cash_balance(self, current_page):
+        _query = self.session.query(DailyReport).limit(self.page_size).offset(current_page * self.page_size)
+        return sum([p.cash_balance for p in _query])
+
+    def total_cash_balance(self):
+        return self.session.query(func.sum(DailyReport.cash_balance)).scalar()
+
+    def page_remainder_of_day(self, current_page):
+        _query = self.session.query(DailyReport).limit(self.page_size).offset(current_page * self.page_size)
+        return sum([p.remainder_of_day for p in _query])
+
+    def total_remainder_of_day(self):
+        return self.session.query(func.sum(DailyReport.remainder_of_day)).scalar()
+
+    def page_cashless(self, current_page):
+        _query = self.session.query(DailyReport).limit(self.page_size).offset(current_page * self.page_size)
+        return sum([p.cashless for p in _query])
+
+    def total_cashless(self):
+        return self.session.query(func.sum(DailyReport.cashless)).scalar()
+
+    def page_actual_balance(self, current_page):
+        _query = self.session.query(DailyReport).limit(self.page_size).offset(current_page * self.page_size)
+        return sum([p.actual_balance for p in _query])
+
+    def total_actual_balance(self):
+        return self.session.query(func.sum(DailyReport.actual_balance)).scalar()
+
+    def render(self, template, **kwargs):
+        # we are only interested in the list page
+        if template == 'admin/model/custom_list.html':
+            # append a summary_data dictionary into kwargs
+            _current_page = kwargs['page']
+            kwargs['summary_data'] = [
+                {'title': 'На странице', 'name': None, 'cashbox': self.page_cashbox(_current_page)},
+                {'title': 'Полная сумма', 'name': None, 'cashbox': self.total_cashbox()},
+                {'title': 'На странице', 'name': None, 'cash_balance': self.page_cash_balance(_current_page)},
+                {'title': 'Полная сумма', 'name': None, 'cash_balance': self.total_cash_balance()},
+                {'title': 'На странице', 'name': None, 'remainder_of_day': self.page_remainder_of_day(_current_page)},
+                {'title': 'Полная сумма', 'name': None, 'remainder_of_day': self.total_remainder_of_day()},
+                {'title': 'На странице', 'name': None, 'cashless': self.page_cashless(_current_page)},
+                {'title': 'Полная сумма', 'name': None, 'cashless': self.total_cashless()},
+                {'title': 'На странице', 'name': None, 'actual_balance': self.page_actual_balance(_current_page)},
+                {'title': 'Полная сумма', 'name': None, 'actual_balance': self.total_actual_balance()},
+            ]
+        return super(DailyReportAdmin, self).render(template, **kwargs)
+
+
+class RoleAdmin(ModelView):
+    column_labels = dict(
+        name='Название',
+        description='Описание',
+        barista='Бариста'
+    )
+    column_formatters = dict(name=lambda v, c, m, p: m.name.title())
