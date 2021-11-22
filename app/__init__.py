30ee1e7c8ea58ffba904604a9b337a8828db1b89
Oleg Repalo <altwulf7@gmail.com>
2021-11-22 22:05:39 +0200

    Fix accordion on shop view, and move exit link in admin panel to nav bar

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import os
 import logging
 from logging.handlers import RotatingFileHandler
-from flask import Flask, request, session
+from flask import Flask, request, url_for
 from flask_babelex import Babel
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -11,6 +11,7 @@ from flask_moment import Moment
 from flask_security import Security, SQLAlchemyUserDatastore
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_admin import helpers as admin_helpers
+from flask_admin.menu import MenuLink
 from flask_modals import Modal
 from flask_babelex import lazy_gettext as _l
 from config import DevelopmentConfig
@@ -56,18 +57,19 @@ app.register_blueprint(user)
 app.register_blueprint(menu)
 app.register_blueprint(report)
 
-admin = Admin(app, name='Not Detail Poster', template_mode='bootstrap4', index_view=IndexAdmin(name='Обзор'))
-admin.add_view(ShopAdmin(models.Shop, db.session, name='Все кофейни', category="Кофейни"))
-admin.add_view(ShopEquipmentAdmin(models.ShopEquipment, db.session, name='Оборудование', category="Кофейни"))
-admin.add_view(StorageAdmin(models.Storage, db.session, name='Все товары', category="Склады"))
-admin.add_view(ReportAdmin(models.Report, db.session, name='Отчеты', category='Статистика'))
-admin.add_view(SupplyAdmin(models.Supply, db.session,  name='Поступления', category="Движения товаров"))
-admin.add_view(ByWeightAdmin(models.ByWeight, db.session,  name='Развес', category="Движения товаров"))
-admin.add_view(WriteOffAdmin(models.WriteOff, db.session,  name='Списания', category="Движения товаров"))
-admin.add_view(ExpenseAdmin(models.Expense, db.session, name='Расходы', category='Кассовые средства'))
-admin.add_view(BaristaAdmin(models.Barista, db.session, name='Сотрудники'))
-admin.add_view(CategoryAdmin(models.Category, db.session, name='Категории', category='Разное'))
-admin.add_view(RoleAdmin(models.Role, db.session, name='Доступ', category='Разное'))
+admin = Admin(app, name='Not Detail Poster', template_mode='bootstrap4', index_view=IndexAdmin(name=_l('Обзор')))
+admin.add_view(ShopAdmin(models.Shop, db.session, name=_l('Кофейни'), category=_l("Кофейни")))
+admin.add_view(ShopEquipmentAdmin(models.ShopEquipment, db.session, name=_l('Оборудование'), category=_l("Кофейни")))
+admin.add_view(StorageAdmin(models.Storage, db.session, name=_l('Товары'), category=_l("Кофейни")))
+admin.add_view(ReportAdmin(models.Report, db.session, name=_l('Отчеты'), category=_l('Статистика')))
+admin.add_view(SupplyAdmin(models.Supply, db.session,  name=_l('Поступления'), category=_l("Движения товаров")))
+admin.add_view(ByWeightAdmin(models.ByWeight, db.session,  name=_l('Развес'), category=_l("Движения товаров")))
+admin.add_view(WriteOffAdmin(models.WriteOff, db.session,  name=_l('Списания'), category=_l("Движения товаров")))
+admin.add_view(ExpenseAdmin(models.Expense, db.session, name=_l('Расходы'), category=_l('Кассовые средства')))
+admin.add_view(BaristaAdmin(models.Barista, db.session, name=_l('Сотрудники')))
+admin.add_view(CategoryAdmin(models.Category, db.session, name=_l('Категории'), category=_l('Разное')))
+admin.add_view(RoleAdmin(models.Role, db.session, name=_l('Доступ'), category=_l('Разное')))
+admin.add_link(MenuLink(name=_l('Выход'), url='/index'))
 
 
 @security.context_processor
