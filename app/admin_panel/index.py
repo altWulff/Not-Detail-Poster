ff7d4b470b751abeb603027accfbf5b7bf32dea5
altWulff <altwulf7@gmail.com>
2022-01-11 17:45:58 +0200

    Add query permissions on index page on admin panel, and shop view on index page

--- app/admin_panel/index.py
+++ app/admin_panel/index.py
@@ -1,7 +1,7 @@
 from flask import redirect, url_for
 from flask_admin import AdminIndexView, expose
 from flask_security import current_user
-from app.models import Shop, Expense, Supply
+from app.models import Shop, Storage, Expense, Supply
 
 
 class IndexAdmin(AdminIndexView):
@@ -15,16 +15,45 @@ class IndexAdmin(AdminIndexView):
             pass
         return False
 
+    def staff_shops_id(self):
+        shop_ids = (shop.id for shop in current_user.shop)
+        return shop_ids
+        
+    def staff_storage_id(self):
+        storage_ids = (storage.id for storage in current_user.storage)
+        return storage_ids
+
+    def shop_query(self):
+        _query = Shop.query
+        if not current_user.has_role('admin'):
+            _query = _query.filter(Shop.id.in_(self.staff_shops_id()))
+        return _query
+        
+    def expense_query(self, type_cost):
+        _query = Expense.query.filter_by(type_cost=type_cost)
+        shop_id = Expense.shop_id
+        if not current_user.has_role('admin'):
+            _query = _query.filter(shop_id.in_(self.staff_shops_id()))
+        return _query
+        
+    def supply_query(self, type_cost):
+        _query = Supply.query.filter_by(type_cost=type_cost)
+        if not current_user.has_role('admin'):
+            _query = _query.join(Supply.storage).filter(Storage.id.in_(self.staff_storage_id()))
+        return _query
+
     @expose('/', methods=('GET', 'POST'))
     def index(self):
         if not self.can_view:
             return redirect(url_for('home'))
         template = 'admin/index.html'
-        _query_exp_cash = Expense.query.filter_by(type_cost='cash').all()
-        _query_exp_cashless = Expense.query.filter_by(type_cost='cashless').all()
-        _query_supply_cash = Supply.query.filter_by(type_cost='cash').all()
-        _query_supply_cashless = Supply.query.filter_by(type_cost='cashless').all()
-        _query_shop = Shop.query.all()
+        
+        _query_exp_cash = self.expense_query(type_cost='cash').all()
+        _query_exp_cashless = self.expense_query(type_cost='cashless').all()
+        _query_supply_cash = self.supply_query(type_cost='cash').all()
+        _query_supply_cashless = self.supply_query(type_cost='cashless').all()
+        _query_shop = self.shop_query().all()
+        
         kwargs = dict()
         kwargs['shop_cash'] = sum([s.cash for s in _query_shop])
         kwargs['shop_cashless'] = sum([s.cashless for s in _query_shop])
