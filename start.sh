a88db999ea714904cecd155bce121c18a619a687
Oleg Repalo <altwulf7@gmail.com>
2022-04-14 00:17:30 +0300

    Fix typos, add installation instructions

---
+++ start.sh
@@ -0,0 +1,10 @@
+export FLASK_APP=not_detail_poster.py
+export FLASK_ENV=development
+export SECRET_KEY='super_strong_key'
+export DATABASE_URL="postgresql:///dev_db"
+createdb dev_db
+flask db upgrade
+flask translate compile
+flask create roles
+flask create superuser admin admin
+flask run
